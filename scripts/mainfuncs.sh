0ac89239e2606e5f85f4632ec30baa2a4e504464
Tong Li <litong01@us.ibm.com>
2020-01-05 05:05:51 +0000

    Fixed chaincode instantiate bug
    Signed-off-by: Tong Li <litong01@us.ibm.com>

--- scripts/mainfuncs.sh
+++ scripts/mainfuncs.sh
@@ -15,7 +15,7 @@ function isValidateOp() {
 # Print the usage message
 function printHelp() {
   echo "Usage: "
-  echo "  minifab.sh <mode> [-c <channel name>] [-s <dbtype>] [-l <language>] [-i <imagetag>] [-n <cc name>] [-v <cc version>]"
+  echo "  minifab.sh <mode> [-c <channel name>] [-s <dbtype>] [-l <language>] [-i <imagetag>] [-n <cc name>] [-v <cc version>] [-p <instantiate parameters>]"
   echo "    <mode> - one of 'up', 'down', 'restart', 'generate', 'install', 'instantiate', 'create' or 'join'"
   echo "      - 'up' - bring up the network with docker-compose up"
   echo "      - 'down' - clear the network with docker-compose down"
@@ -27,20 +27,20 @@ function printHelp() {
   echo "      - 'join'  - join all peers currently in the network to a channel"
   echo "    -c <channel name> - channel name to use (defaults to \"mychannel\")"
   echo "    -s <dbtype> - the database backend to use: goleveldb (default) or couchdb"
-  echo "    -l <language> - the programming language of the chaincode to deploy: go (default), javascript, or java"
-  echo "    -i <imagetag> - the tag to be used to launch the network (defaults to \"latest\")"
+  echo "    -l <language> - the programming language of the chaincode to deploy: go (default), node, or java"
+  echo "    -i <imagetag> - the tag to be used to launch the network (defaults to \"1.4.4\")"
   echo "    -n <chaincode name> - chaincode name to be installed"
   echo "    -v <chaincode version> - chaincode version"
+  echo "    -p <instantiate parameters> - chaincode instantiation parameters"
   echo "  minifab.sh -h (print this message)"
   echo
   echo "Typically, one would first generate the required certificates and "
   echo "genesis block, then bring up the network. e.g.:"
   echo
   echo "	minifab.sh generate -c mychannel"
-  echo "	minifab.sh up -c mychannel -s couchdb"
-  echo "        minifab.sh up -c mychannel -s couchdb -i 1.4.0"
-  echo "	minifab.sh up -l javascript"
-  echo "	minifab.sh down -c mychannel"
+  echo "	minifab.sh up -c mychannel"
+  echo "  minifab.sh up -s couchdb -i 1.4.0"
+  echo "	minifab.sh down"
   echo
   echo "Taking all defaults:"
   echo "	minifab.sh generate"
@@ -54,7 +54,7 @@ function networkUp() {
   -e "hostroot=$(pwd)" -e "regcerts=false" -e "CC_LANGUAGE=$CC_LANGUAGE"              \
   -e "DB_TYPE=$DB_TYPE" -e "CHANNEL_NAME=$CHANNEL_NAME" -e "CC_NAME=$CC_NAME"         \
   -e "CC_VERSION=$CC_VERSION" -e "CHANNEL_NAME=$CHANNEL_NAME" -e "IMAGETAG=$IMAGETAG" \
-  plays/minifabric.yaml
+  -e "CC_PARAMETERS=$CC_PARAMETERS" plays/minifabric.yaml
 }
 
 function networkDown() {
@@ -63,7 +63,7 @@ function networkDown() {
   -e "mode=destroy" -e "removecert=false" -e "CC_LANGUAGE=$CC_LANGUAGE"               \
   -e "DB_TYPE=$DB_TYPE" -e "CHANNEL_NAME=$CHANNEL_NAME" -e "CC_NAME=$CC_NAME"         \
   -e "CC_VERSION=$CC_VERSION" -e "CHANNEL_NAME=$CHANNEL_NAME" -e "IMAGETAG=$IMAGETAG" \
-  plays/minifabric.yaml
+  -e "CC_PARAMETERS=$CC_PARAMETERS" plays/minifabric.yaml
 }
 
 function generateCerts() {
@@ -71,7 +71,7 @@ function generateCerts() {
   -i plays/hosts -e "mode=apply" -e "regcerts=true" -e "CC_LANGUAGE=$CC_LANGUAGE"     \
   -e "DB_TYPE=$DB_TYPE" -e "CHANNEL_NAME=$CHANNEL_NAME" -e "CC_NAME=$CC_NAME"         \
   -e "CC_VERSION=$CC_VERSION" -e "CHANNEL_NAME=$CHANNEL_NAME" -e "IMAGETAG=$IMAGETAG" \
-  plays/minifabric.yaml --skip-tags "nodes"
+  -e "CC_PARAMETERS=$CC_PARAMETERS" plays/minifabric.yaml --skip-tags "nodes"
 }
 
 function doOp() {
@@ -80,5 +80,5 @@ function doOp() {
   -e "mode=$1" -e "hostroot=$(pwd)" -e "CC_LANGUAGE=$CC_LANGUAGE"                     \
   -e "DB_TYPE=$DB_TYPE" -e "CHANNEL_NAME=$CHANNEL_NAME" -e "CC_NAME=$CC_NAME"         \
   -e "CC_VERSION=$CC_VERSION" -e "CHANNEL_NAME=$CHANNEL_NAME" -e "IMAGETAG=$IMAGETAG" \
-  plays/fabops.yaml
+  -e "CC_PARAMETERS=$CC_PARAMETERS" plays/fabops.yaml
 }
