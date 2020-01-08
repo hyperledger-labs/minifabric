c8564a2d2d362f0aeeb00c3720d1f8145ed4a148
Tong Li <litong01@us.ibm.com>
2020-01-08 18:06:47 +0000

    Select right chaincode based on fabric release
    Signed-off-by: Tong Li <litong01@us.ibm.com>

--- minifab.sh
+++ minifab.sh
@@ -73,7 +73,14 @@ CC_PARAMETERS=$(echo $CC_PARAMETERS|base64)
 
 if [ -z "$hostroot" ]; then hostroot=$(pwd); fi
 echo "hostroot: $hostroot"
-[ ! -d "$(pwd)/vars/chaincode" ] && cp -r $(pwd)/chaincode $(pwd)/vars
+if [ ! -d "$(pwd)/vars/chaincode" ]; then
+  mkdir -p $(pwd)/vars/chaincode/simple
+  if [[ "$IMAGETAG" = "2.0" ]] || [[ "$IMAGETAG" > "2.0" ]]; then
+    cp -r $(pwd)/chaincode/simple2/* $(pwd)/vars/chaincode/simple
+  else
+    cp -r $(pwd)/chaincode/simple/* $(pwd)/vars/chaincode/simple
+  fi
+fi
 
 if [ "${MODE}" == "up" ]; then
   time networkUp
