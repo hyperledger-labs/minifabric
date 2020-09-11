89e4ebf6cd9b1e0da9c891d572e65b4be759c3a8
Tong Li <litong01@us.ibm.com>
2020-09-11 14:50:04 +0000

    Added sample app to deal with CA
    Signed-off-by: Tong Li <litong01@us.ibm.com>

--- app/main.go
+++ app/main.go
@@ -4,10 +4,12 @@ import (
 	"fmt"
 	"github.com/google/uuid"
 	"github.com/hyperledger/fabric-sdk-go/pkg/client/channel"
+	"github.com/hyperledger/fabric-sdk-go/pkg/client/msp"
 	"github.com/hyperledger/fabric-sdk-go/pkg/common/errors/retry"
 	"github.com/hyperledger/fabric-sdk-go/pkg/core/config"
 	"github.com/hyperledger/fabric-sdk-go/pkg/fabsdk"
 	"github.com/hyperledger/fabric-sdk-go/pkg/gateway"
+	rmsp "github.com/hyperledger/fabric-sdk-go/pkg/msp"
 	"math/rand"
 	"reflect"
 	"strconv"
@@ -21,6 +23,72 @@ const (
 	orgName   = "org1.example.com"
 	orgAdmin  = "Admin"
 )
+
+func doEnroll() {
+	//Load configuration from connection profile
+	cnfg := config.FromFile("./mychannel_connection_for_gosdk.json")
+	sdk, err := fabsdk.New(cnfg)
+	if err != nil {
+		fmt.Printf("Failed to create new SDK: %s", err)
+		return
+	}
+	defer sdk.Close()
+
+	// Try to get some configuration data from the connection profile
+	sdkcfg, _ := sdk.Config()
+	idcfg, _ := rmsp.ConfigFromBackend(sdkcfg)
+	caconfig, ok := idcfg.CAConfig("ca1.org1.example.com")
+	if !ok {
+		fmt.Println("Could not get the caconfiguration.")
+		return
+	}
+	fmt.Println(caconfig.Registrar.EnrollID)
+	fmt.Println(caconfig.Registrar.EnrollSecret)
+
+	ctxProvider := sdk.Context()
+	mspClient, err := msp.New(ctxProvider)
+	if err != nil {
+		fmt.Printf("Failed to create new msp client: %s", err)
+		return
+	}
+
+	// Now try to enroll the admin with its configured ID and password
+	err = mspClient.Enroll(caconfig.Registrar.EnrollID, msp.WithSecret(caconfig.Registrar.EnrollSecret))
+	if err != nil {
+		fmt.Printf("Failed to enroll the admin: %s", err)
+		return
+	}
+	fmt.Println("Enrollment is fine")
+
+	// Try to query all identities
+	fmt.Println(reflect.TypeOf(mspClient))
+	ids, err := mspClient.GetAllIdentities()
+	if err != nil {
+		fmt.Printf("Failed to get all ids: %s", err)
+		return
+	}
+
+	for _, id := range ids {
+		fmt.Printf("an id %v", id.ID)
+	}
+
+	//Register a new user
+	username := "user" + strconv.Itoa(rand.Intn(500000))
+	userpw, err := mspClient.Register(&msp.RegistrationRequest{Name: username})
+	if err != nil {
+		fmt.Printf("Failed to get all ids: %s", err)
+		return
+	}
+	fmt.Printf("The new user %v", userpw)
+
+	//Enroll with returned password
+	err = mspClient.Enroll(username, msp.WithSecret(userpw))
+	if err != nil {
+		fmt.Printf("Failed to enroll the admin: %s", err)
+		return
+	}
+	fmt.Println("Enrollment is fine")
+}
 
 func useClientExecute(index int) {
 	cnfg := config.FromFile("./mychannel_connection_for_gosdk.json")
